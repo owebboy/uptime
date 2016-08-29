24b31743779af624a543b79e24459eaed0657d5f
Oliver Pope <opope@me.com>
2016-08-28 22:57:17 -0400

    highway to the danger zone

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+module.exports = function(io) {
+  var express = require('express');
+  var router = express.Router();
+
+  var ping = require('ping');
+  var hosts = ['webtype.io', 'finance.galleryonthealley.net', 'owebboy.com'];
+
+  io.on('connection', function(socket) {
+    setInterval(function() {
+      hosts.forEach(function (host) {
+          ping.promise.probe(host)
+            .then(function (res) {
+              socket.emit('update', { host: host, res: res });
+            });
+      });
+    }, 1000);
+  });
+
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'owebboy uptime' });
+  });
+
+  /* GET table page. */
+  router.get('/table', function(req, res, next) {
+    res.render('table', { title: 'owebboy uptime' });
+  });
+
+  return router;
+}
